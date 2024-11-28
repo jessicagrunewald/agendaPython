fa277027e1b746ded0798d1c19109eb111745ce1
jessicagrunewald <j_grunewald@hotmail.com>
2024-11-28 11:15:35 -0300

    Configuração do arquivo routes

---
+++ app/routes.py
@@ -0,0 +1,12 @@
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from app.db import get_db_connection
+
+app = Blueprint('routes', __name__)
+
+@app.route('/')
+def index():
+    # Página inicial: lista todos os agendamentos
+    conn = get_db_connection()
+    agendamentos = conn.execute('SELECT * FROM agenda ORDER BY data, horario').fetchall()
+    conn.close()
+    return render_template('index.html', agendamentos=agendamentos)
