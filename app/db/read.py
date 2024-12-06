7e045ed0498a5cb81d3c5151b0147740b89675be
jessicagrunewald <j_grunewald@hotmail.com>
2024-12-06 15:01:29 -0300

    Função read - ler agendamento específico

--- app/db/read.py
+++ app/db/read.py
@@ -7,3 +7,12 @@ def get_all_agendamentos():
     agendamentos = cursor.fetchall()
     conn.close()
     return agendamentos
+
+# Ler um agendamento específico
+def get_agendamento_by_id(agendamento_id):
+    conn = get_db_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM agenda WHERE id = ?", (agendamento_id,))
+    agendamento = cursor.fetchone()
+    conn.close()
+    return agendamento
