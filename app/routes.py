6d6f2c7bd5c88ffd7444067d9055393d2921d776
jessicagrunewald <j_grunewald@hotmail.com>
2024-12-09 13:39:20 -0300

    Alterações nos templates html e ajuste nas rotas

--- app/routes.py
+++ app/routes.py
@@ -6,17 +6,22 @@ from app.db.update import update_agendamento
 
 app = Blueprint('routes', __name__)
 
+# Página inicial: lista todos os agendamentos
 @app.route('/')
 def index():
-    # Página inicial: lista todos os agendamentos
     conn = get_db_connection()
     agendamentos = conn.execute('SELECT * FROM agenda ORDER BY data, horario').fetchall()
     conn.close()
     return render_template('index.html', agendamentos=agendamentos)
 
-# Rota função create
-@app.route('/add', methods=['POST'])
-def add():
+# Rota GET para exibir o formulário de adição de agendamentos
+@app.route('/create', methods=['GET'])
+def show_create_form():
+    return render_template('create.html')
+
+# Rota POST para adicionar um novo agendamento
+@app.route('/create', methods=['POST'])
+def create():
     titulo = request.form['titulo']
     data = request.form['data']
     horario = request.form['horario']
@@ -28,18 +33,25 @@ def add():
 
     add_agendamento(titulo, data, horario, descricao, participantes, emails, link, notas)
     flash("Agendamento adicionado com sucesso!")
-    return redirect(url_for('index'))
+    return redirect(url_for('routes.index'))
 
 # Rota função delete
 @app.route('/delete/<int:id>', methods=['POST'])
 def delete(id):
     delete_agendamento(id)
     flash("Agendamento deletado com sucesso!")
-    return redirect(url_for('index'))
+    return redirect(url_for('routes.index'))
 
 # Rota função update
-@app.route('/edit/<int:id>', methods=['POST'])
-def edit(id):
+@app.route('/update/<int:id>', methods=['GET'])
+def show_update_form(id):
+    conn = get_db_connection()
+    agendamento = conn.execute('SELECT * FROM agenda WHERE id = ?', (id,)).fetchone()
+    conn.close()
+    return render_template('update.html', agendamento=agendamento)
+
+@app.route('/update/<int:id>', methods=['POST'])
+def update(id):
     titulo = request.form['titulo']
     data = request.form['data']
     horario = request.form['horario']
@@ -51,4 +63,4 @@ def edit(id):
 
     update_agendamento(id, titulo, data, horario, descricao, participantes, emails, link, notas)
     flash("Agendamento atualizado com sucesso!")
-    return redirect(url_for('index'))
+    return redirect(url_for('routes.index'))
